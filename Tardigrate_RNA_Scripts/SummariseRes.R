7a38f443f88c64fb8b415b2e582e21f2c1bb776f
Jazmín Ramos Madrigal <51083184+JazminRM@users.noreply.github.com>
2020-10-17 14:54:17 +0200

    Add files via upload

---
+++ Tardigrate_RNA_Scripts/SummariseRes.R
@@ -0,0 +1,90 @@
+####################################
+# Summarise the results from the three DE methods
+#
+####################################
+
+# R
+
+# Read files
+# Here add the names of the cvs files where you have the results from each of the three methods used:
+m1<-read.csv("results_method1.csv")
+m2<-read.csv("results_method1.csv")
+m3<-read.csv("results_method1.csv")
+
+
+# read annotation files (these ones are attached in the email)
+annotation_info1<-read.table("Ro-001-Unigene.fa.blast.swissprot.txt", as.is=T, sep="\t", h=T)
+annotation_info2<-read.table("Ro001UnigeneGene2GOedited", as.is=T, sep="\t")
+
+# the 1 inside the brackets correspond to the column that contain the gene id (unigene or contig ID), so if it is not column 1 then change accordingly
+DE_genes<-unique(m1[,1], m2[,1], m3[,1])
+
+m1_p<-NULL
+m2_p<-NULL
+m3_p<-NULL
+gene_id<-NULL
+GO_id<-NULL
+descrip<-NULL
+m1_FDR<-NULL
+m2_FDR<-NULL
+m3_FDR<-NULL
+for(i in 1:length(DE_genes)){
+	# change here the 1 if that column doesn't corresponds to the gene id
+	if(sum(m1[,1]==DE_genes[i])>0){
+		# Here you could change "method 1" for the name of the program you used (e.g. limma, DESeq)
+		m1_p<-c(m1_p, "Method 1")
+		# Here the 1 in "m1[,1]" should be the column that contains the gene id, and the 2 in "[i],2]" should be the column that contains the FDR
+		m1_FDR<-c(m1_FDR, m1[m1[,1]==DE_genes[i],2])
+	}else{
+		m1_p<-c(m1_p, NA)
+		m1_FDR<-c(m1_FDR, NA)
+	}
+
+	# change here the 1 if that column doesn't corresponds to the gene id
+	if(sum(m2[,1]==DE_genes[i])>0){
+		# Here you could change "method 2" for the name of the program too
+		m2_p<-c(m2_p, "Method 2")
+		# Here the 1 in "m2[,1]" should be the column that contains the gene id, and the 2 in "[i],2]" should be the column that contains the FDR
+		m2_FDR<-c(m2_FDR, m2[m2[,1]==DE_genes[i],2])
+
+	}else{
+		m2_p<-c(m2_p, NA)
+		m2_FDR<-c(m2_FDR, NA)
+	}
+
+	# change here the 1 if that column doesn't corresponds to the gene id
+	if(sum(m3[,1]==DE_genes[i])>0){
+		# Here you could change "method 1" for the name of the program too
+		m3_p<-c(m3_p, "Method 3")
+		# Here the 1 in "m3[,1]" should be the column that contains the gene id, and the 2 in "[i],2]" should be the column that contains the FDR
+		m3_FDR<-c(m3_FDR, m3[m3[,1]==DE_genes[i],2])
+
+	}else{
+		m3_p<-c(m3_p, NA)
+		m3_FDR<-c(m3_FDR, NA)
+	}
+	
+	if(sum(annotation_info1[,1]==DE_genes[i])>0){
+		temp<-annotation_info1[annotation_info1[,1]==DE_genes[i],2]
+		gene_id<-c(gene_id, temp[1])
+		temp<-annotation_info1[annotation_info1[,1]==DE_genes[i],13]
+		descrip<-c(descrip, temp[1])
+	}else{
+		gene_id<-c(gene_id, NA)
+		descrip<-c(descrip, NA)
+	}
+	if(sum(annotation_info2[,1]==DE_genes[i])>0){
+		GO_id<-c(GO_id, annotation_info2[annotation_info2[,1]==DE_genes[i],2])
+	}	
+}
+
+tab<-cbind(DE_genes, m1_p, m1_FDR, m2_p, m2_FDR, m3_p, m3_FDR, gene_id, GO_id, descrip)
+
+colnames(tab)<-c("Seq_id", "m1", "m1_FDR", "m2", "m2_FDR", "m3", "m3_FDR", "gene_id", "GO_kegg", "Description")
+
+write.table(tab, quote=F, sep="\t", col.names=T, row.names=F, file="Summary_table.txt")
+
+####################################
+
+
+
---
+++ Tardigrate_RNA_Scripts/TopGO.R
@@ -0,0 +1,56 @@
+###############################################
+# Enrichment analysis using topGO library
+# 
+
+# Prepare annotation data
+# save BGI_result/3.Annotation/Ro-001-Unigene.fa.Gene2GO.xls as a csv file
+# paste -d "\t" <(cut -f 1 -d ";" Ro001UnigeneGene2GO.csv |tail -n +2) <(cut -f 2- -d ";" Ro001UnigeneGene2GO.csv |tail -n +2|perl -pe 's/;/, /g;' |perl -pe 's/ , ,//g;' |perl -pe 's/, ,//g;' |perl -pe 's/, \n/\n/g;' |perl -pe 's/,\n/\n/g;' ) > Ro001UnigeneGene2GOedited
+
+
+# Set directory #
+setwd("C:/Users/mch497/Dropbox/Carrot/DE/Blast2Go/TopGO")
+
+library("topGO")
+library("Rgraphviz")
+
+# read annotation data 
+geneID2GO <- readMappings("Ro001UnigeneGene2GOedited", sep = "\t", IDsep = ",")
+str(head(geneID2GO))
+
+# Predefine a list of DE genes #
+geneNames <- names(geneID2GO)
+head(geneNames)
+genesOfInterest<-readLines("ListofInterestingGenes.txt")
+
+geneList <- factor(as.integer(geneNames %in% genesOfInterest)) #binary list indicating the DE genes
+names(geneList) <- geneNames
+str(geneList)
+
+# create an topGO data object #
+myGOdata <- new("topGOdata", ontology = "MF", allGenes = geneList, annot = annFUN.gene2GO, gene2GO = geneID2GO)
+
+# run the Fisher's exact tests
+resultClassic <- runTest(myGOdata, algorithm="classic", statistic="fisher")
+resultElim <- runTest(myGOdata, algorithm="elim", statistic="fisher")
+resultTopgo <- runTest(myGOdata, algorithm="weight01", statistic="fisher")
+resultParentchild <- runTest(myGOdata, algorithm="parentchild", statistic="fisher")
+
+# see how many results we get where weight01 gives a P-value <= 0.05:
+mysummary <- summary(attributes(resultTopgo)$score <= 0.05)
+numsignif <- as.integer(mysummary[[3]]) # how many terms is it true that P <= 0.05
+
+# create and write a table summarising the top 'numsignif' results:
+allRes <- GenTable(myGOdata, classicFisher = resultClassic, elimFisher = resultElim, topgoFisher = resultTopgo, parentchildFisher = resultParentchild, orderBy = "topgoFisher", ranksOf = "classicFisher", topNodes = numsignif)
+
+write.csv(allRes, "TopGO_Results_DE.csv")
+
+# print a graph (to a pdf file) with the top 'numsignif' results:
+printGraph(myGOdata, resultTopgo, firstSigNodes = numsignif, fn.prefix = "topGO", useInfo = "all", pdfSW = TRUE)
+dev.off()
+
+# check topGO manual
+
+browseVignettes("topGO")
+
+
+
