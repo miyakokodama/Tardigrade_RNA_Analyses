01a89a588f17d9f50397b170af1b205525f98ba7
Miyako Kodama <miyakokodama@gmail.com>
2021-12-20 15:09:08 +0100

    Update 3.Make_Genecount_File_From_Salmon_16Sep20.sh

--- Tardigrate_RNA_Scripts/3.Make_Genecount_File_From_Salmon_16Sep20.sh
+++ Tardigrate_RNA_Scripts/3.Make_Genecount_File_From_Salmon_16Sep20.sh
@@ -1,4 +1,6 @@
-############
+################################################
+# make a gene count file including all samples 
+################################################
 
 cut -f1 T_1_quant.sf > genename
 for f in T_1_quant.sf T_2_quant.sf T_3_quant.sf T_4_quant.sf T_5_quant.sf T_6_quant.sf T_7_quant.sf T_8_quant.sf T_9_quant.sf T_10_quant.sf T_11_quant.sf T_12_quant.sf T_13_quant.sf T_14_quant.sf T_15_quant.sf T_16_quant.sf T_17_quant.sf T_18_quant.sf 
@@ -8,10 +10,11 @@ do
 done
 paste genename T_1_quant.sf.tmp T_2_quant.sf.tmp T_3_quant.sf.tmp T_4_quant.sf.tmp T_5_quant.sf.tmp T_6_quant.sf.tmp T_7_quant.sf.tmp T_8_quant.sf.tmp T_9_quant.sf.tmp T_10_quant.sf.tmp T_11_quant.sf.tmp T_12_quant.sf.tmp T_13_quant.sf.tmp T_14_quant.sf.tmp T_15_quant.sf.tmp T_16_quant.sf.tmp T_17_quant.sf.tmp T_18_quant.sf.tmp > genecount
 
-###########
-# convert a file to .csv
+#########################################
+# convert athe gene count table to .csv
+#########################################
+
 cat genecount | tr -s '[:blank:]' ',' > genecount.csv
 
-# remove quant.sf (make it from "T_1_quant.sf" -> "T_1")
-# https://www.theunixschool.com/2014/08/sed-examples-remove-delete-chars-from-line-file.html
+# remove trailing "quant.sf" text from each sample (make it from "T_1_quant.sf" -> "T_1")
 sed 's/_quant.sf//' genecount.csv > genecount.csv
